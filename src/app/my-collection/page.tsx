ef69c2b08232a754c9bb23ffac5015decf60734c
Alonso Garay <alonsogarayp@gmail.com>
2023-12-13 22:39:19 -0500

    Initial layout navbar config

---
+++ src/app/my-collection/page.tsx
@@ -0,0 +1,7 @@
+export default function MyCollection () {
+  return (
+    <div>
+      <h1>My Collection</h1>
+    </div>
+  )
+}
---
+++ src/app/ui/navbar/nav-links.tsx
@@ -0,0 +1,31 @@
+'use client'
+import styles from '@/app/page.module.css'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const links = [
+  { name: 'Home', href: '/' },
+  { name: 'My Collection', href: '/my-collection' },
+  { name: 'Cards', href: '/cards' },
+  { name: 'News', href: '/news' }
+]
+
+export default function NavLinks () {
+  const pathname = usePathname()
+
+  return (
+    <>
+      {links.map((link) => {
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={pathname === link.href ? `${styles.active}` : ''}
+          >
+            {link.name}
+          </Link>
+        )
+      })}
+    </>
+  )
+}
