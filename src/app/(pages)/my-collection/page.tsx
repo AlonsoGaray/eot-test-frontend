d20ba5b299e9db5c06aa187e2b1993604a84645a
Alonso Garay <alonsogarayp@gmail.com>
2023-12-17 11:53:50 -0500

    Remove unused code and update API endpoint

--- src/app/(pages)/my-collection/page.tsx
+++ src/app/(pages)/my-collection/page.tsx
@@ -1,9 +1,4 @@
-import { fetchMessage } from '@/app/lib/data'
-
 export default async function MyCollection () {
-  const asd = await fetchMessage()
-  console.log('🚀 ~ file: page.tsx:5 ~ MyCollection ~ asd:', asd)
-
   return (
     <div>
       <h1>My Collection</h1>
--- src/app/lib/data.ts
+++ src/app/lib/data.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export async function fetchLatestTenCards (name: string | null) {
   try {
-    const response = await axios.get('http://localhost:8080/api/cards', {
+    const response = await axios.get('http://localhost:8080/api/card/latest', {
       params: {
         pageSize: 10,
         q: name ? `name:${name}*` : ''
@@ -15,19 +15,5 @@ export async function fetchLatestTenCards (name: string | null) {
   } catch (err) {
     console.log('🚀 ~ file: data.ts:13 ~ fetchLatestTenCards ~ err:', err)
     throw new Error('Failed to fetch latest ten cards')
-  }
-}
-
-export async function fetchMessage () {
-  try {
-    const response = await axios.get('http://localhost:8080/')
-    console.log('🚀 ~ file: data.ts:24 ~ fetchMessage ~ response:', response)
-
-    if (response.status === 200) {
-      return response.data
-    }
-  } catch (err) {
-    console.log('🚀 ~ file: data.ts:13 ~ fetchMessage ~ err:', err)
-    throw new Error('Failed to fetch message')
   }
 }
