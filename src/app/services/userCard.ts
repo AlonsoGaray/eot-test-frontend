c2fd7614b1567a9b5545a783bbde3c2f002b4499
Alonso Garay <alonsogarayp@gmail.com>
2023-12-19 02:44:44 -0500

    Refactor userCard service to use axios for API calls

--- src/app/services/userCard.ts
+++ src/app/services/userCard.ts
@@ -1,29 +1,19 @@
+import axios from 'axios'
+
 export async function addUserCard (userId: string, cardId: string): Promise<any> {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user-card/add`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      userId,
-      cardId
-    })
+  const response = await axios.put(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user-card/add`, {
+    userId,
+    cardId
   })
 
-  return await response.json()
+  return response.data
 }
 
 export async function decreaseUserCard (userId: string, cardId: string): Promise<any> {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user-card/decrease`, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      userId,
-      cardId
-    })
+  const response = await axios.put(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user-card/decrease`, {
+    userId,
+    cardId
   })
 
-  return await response.json()
+  return response.data
 }
