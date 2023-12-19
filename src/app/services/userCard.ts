491d36ab3b14c25f507a4d75c9b21170e562a5dd
Alonso Garay <alonsogarayp@gmail.com>
2023-12-19 02:29:26 -0500

    Update API endpoints in userCard.ts

--- src/app/services/userCard.ts
+++ src/app/services/userCard.ts
@@ -1,5 +1,5 @@
 export async function addUserCard (userId: string, cardId: string): Promise<any> {
-  const response = await fetch('http://localhost:8080/api/user-card/add', {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user-card/add`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
@@ -14,7 +14,7 @@ export async function addUserCard (userId: string, cardId: string): Promise<any>
 }
 
 export async function decreaseUserCard (userId: string, cardId: string): Promise<any> {
-  const response = await fetch('http://localhost:8080/api/user-card/decrease', {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/user-card/decrease`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json'
